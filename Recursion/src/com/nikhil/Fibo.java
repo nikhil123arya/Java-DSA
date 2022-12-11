1383a4147c880617d069c533f12c9e673d0b86a0
nikhil123arya <nikhilakabob@gmail.com>
2022-12-11 23:44:21 +0530

    bitwise operators

--- Recursion/src/com/nikhil/Fibo.java
+++ Recursion/src/com/nikhil/Fibo.java
@@ -2,11 +2,22 @@ package com.nikhil;
 
 public class Fibo {
     public static void main(String[] args) {
-        // to find nth fibonacci number
-        int ans = fibo(4);
-        System.out.println(ans);
+        // to find nth fibonacci number it will take much time to do it
+//        int ans = fibo(4);
+//        System.out.println(ans);
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println(fiboNumber(i));
+        }
+    }
+// using formula of Nth fibonacci number
+    static int fiboNumber(int n) {
+        // just for demo use long instead, ignore the
+//        return (int)((Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
+        return (int)(Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));
     }
 
+    // using recursion
     static int fibo(int n) {
         // base condition
         if (n < 2) {
---
+++ bitwise-operators/src/com/nikhil/FindUinque.java
@@ -0,0 +1,17 @@
+package com.nikhil;
+
+public class FindUinque {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6, 2, 3, 1, 4};
+        System.out.println(ans(arr));
+    }
+
+    private static int ans(int[] arr) {
+        int unique = 0;
+
+        for (int n: arr) {
+            unique ^= n;
+        }
+        return unique;
+    }
+}
